a9db0bb0414f26c1d1a2672919f8f183e332e638
Yamileth Londoño Burgos <142650446+Yamis2706@users.noreply.github.com>
2025-10-31 15:35:51 -0500

    Proyecto
    Creación del Proyecto

---
+++ syncup/src/main/java/co/edu/uniquindio/proyecto/syncup/SyncupApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.proyecto.syncup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncupApplication.class, args);
+	}
+
+}
---
+++ syncup/src/test/java/co/edu/uniquindio/proyecto/syncup/SyncupApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.proyecto.syncup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
